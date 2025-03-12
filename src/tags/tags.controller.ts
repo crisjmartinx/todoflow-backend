5d749100a18b382c7adf3676ef97680e1b8f08ed
Cristian <theiron_maiden_6@hotmail.com>
2025-03-12 01:18:34 -0300

    add auth tags route

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -2,8 +2,10 @@ import { Controller, Get, Post, Body } from '@nestjs/common';
 
 import { TagsService } from './tags.service';
 import { CreateTagDto } from './dto/create-tag.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
 @Controller('tags')
+@Auth()
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
 
